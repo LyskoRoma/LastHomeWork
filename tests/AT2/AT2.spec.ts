226decf955f54efef46e6e6897624c3742876f8d
LyskoRoma <roman.lysko@tripster.ru>
2025-06-02 15:33:21 +0300

    upd notes for AT2

--- tests/AT2/AT2.spec.ts
+++ tests/AT2/AT2.spec.ts
@@ -1,8 +1,8 @@
-import {test, expect} from '@playwright/test';
-import {PageMain} from "@Pages/pageMain";
+import { test, expect } from '@playwright/test';
+import { PageMain } from "@Pages/pageMain";
+import { expectations as exp } from './notes/expectations';
 
-
-test('Проверка авторизации пользователя', async ({page}) => {
+test('Проверка авторизации пользователя', async ({ page }) => {
     await test.step('Перейти на главную страницу JS-learn', async () => {
         await page.goto('/');
     });
@@ -13,37 +13,31 @@ test('Проверка авторизации пользователя', async (
         const modalAuth = pageMain.ModalAuth;
 
         await buttonLogin.click();
-
-        expect(await modalAuth.isVisible({isOpen: true}), 'Отображается модальное окно авторизации').toBe(true);
+        expect(await modalAuth.isVisible({ isOpen: true }), exp.s1.e1).toBe(true);
     });
 
     await test.step('Степ 2: проверить элементы модального окна', async () => {
         const pageMain = new PageMain(page);
         const modalAuth = pageMain.ModalAuth;
 
-
-        expect(await modalAuth.isVisibleTitle(), 'Отображается заголовок "Вход"').toBe(true);
-        expect(await modalAuth.isVisibleButtonRegistration(), 'Отображается кнопка "Регистрация"').toBe(true);
-        expect(await modalAuth.isVisibleInputEmail(), 'Отображается поле "Email"').toBe(true);
-        expect(await modalAuth.isVisibleInputPassword(), 'Отображается поле "Пароль"').toBe(true);
-        expect(await modalAuth.isVisibleButtonForgotPassword(), 'Отображается кнопка "Забыли?"').toBe(true);
-        expect(await modalAuth.isVisibleButtonSubmit(), 'Отображается кнопка "Войти"').toBe(true);
-        expect(await modalAuth.isVisibleButtonClose(), 'Отображается кнопка крестик').toBe(true);
-        expect(await modalAuth.isVisibleBlockLoginFrom(), 'Отображается блок "Вход через социальные сети"').toBe(true);
-
-
+        expect(await modalAuth.isVisibleTitle(), exp.s2.e1).toBe(true);
+        expect(await modalAuth.isVisibleButtonRegistration(), exp.s2.e2).toBe(true);
+        expect(await modalAuth.isVisibleInputEmail(), exp.s2.e3).toBe(true);
+        expect(await modalAuth.isVisibleInputPassword(), exp.s2.e4).toBe(true);
+        expect(await modalAuth.isVisibleButtonForgotPassword(), exp.s2.e5).toBe(true);
+        expect(await modalAuth.isVisibleButtonSubmit(), exp.s2.e6).toBe(true);
+        expect(await modalAuth.isVisibleButtonClose(), exp.s2.e7).toBe(true);
+        expect(await modalAuth.isVisibleBlockLoginFrom(), exp.s2.e8).toBe(true);
     });
 
     await test.step('Степ 3: нажать на крестик в модальном окне', async () => {
         const pageMain = new PageMain(page);
         const modalAuth = pageMain.ModalAuth;
 
-        await modalAuth.clickButtonClose()
-        await page.waitForTimeout(3000)
+        await modalAuth.clickButtonClose();
+        await page.waitForTimeout(3000);
 
-
-        expect(await modalAuth.isVisible({isOpen: false}), 'НЕ отображается модальное окно авторизации').toBe(false);
-
+        expect(await modalAuth.isVisible({ isOpen: false }), exp.s3.e1).toBe(false);
     });
 
     await test.step('Степ 4: В хедере нажать на кнопку авторизации', async () => {
@@ -52,22 +46,20 @@ test('Проверка авторизации пользователя', async (
         const modalAuth = pageMain.ModalAuth;
 
         await buttonLogin.click();
-
-        expect(await modalAuth.isVisible({isOpen: true}), 'Отображается модальное окно авторизации').toBe(true);
+        expect(await modalAuth.isVisible({ isOpen: true }), exp.s4.e1).toBe(true);
     });
 
     await test.step('Степ 5: заполнить поля email / пароль и нажать на кнопку "Войти"', async () => {
         const pageMain = new PageMain(page);
         const header = pageMain.Header;
         const modalAuth = pageMain.ModalAuth;
-        const name =  "Роман Лыско"
+        const name = "Роман Лыско";
 
         await modalAuth.fillEmail();
         await modalAuth.fillPassword();
-        await modalAuth.clickButtonSubmit()
+        await modalAuth.clickButtonSubmit();
 
-        expect(await modalAuth.isVisible({isOpen: false}), 'НЕ отображается модальное окно авторизации').toBe(true);
-        expect(await header.getRegistrationName(), 'В хедере отображается имя зарегестрированного пользователя "Роман Лыско"').toBe(name)
+        expect(await modalAuth.isVisible({ isOpen: false }), exp.s5.e1).toBe(true);
+        expect(await header.getRegistrationName(), exp.s5.e2).toBe(name);
     });
-
 });
